bae48176efcd1e8fca5eed7d3f132cd06bd239ec
Natalia Kirdis <n.kirdis@propellerads.net>
2022-12-06 20:12:32 +0100

    remove useless import

--- src/test/java/com/demoqa/tests/BookStoreWebTests.java
+++ src/test/java/com/demoqa/tests/BookStoreWebTests.java
@@ -3,7 +3,6 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
